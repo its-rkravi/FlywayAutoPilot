95ee3725b922303a984a3e3102df1654ba68caf1
Rahul Kumar Ravi <rkr.rahulravi@gmail.com>
2025-06-06 21:11:51 +0530

    two rows added on live server

---
+++ migrations/V1.2__two rows inserted in the user table.sql
@@ -0,0 +1,2 @@
+insert into public.master_user(username,address,created_by) values ('its_rkravi','bijnor, UP',1);
+insert into public.master_user(username,address,created_by) values ('vishu01','meerut,UP',2);
